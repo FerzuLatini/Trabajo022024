87f0beab539aafa1f438c08c485569840b50c48d
FerzuLatini <fernando.zuniga2@virginiogomez.cl>
2024-10-13 17:40:10 -0300

    trabajo terminado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,15 +4,14 @@ plugins {
 
 android {
     namespace = "cl.virginiogomez.trabajo022024"
-    compileSdk = 34
+    compileSdk = 35  // Asegúrate de que sea 35
 
     defaultConfig {
         applicationId = "cl.virginiogomez.trabajo022024"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35  // Cambiar a 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -25,6 +24,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -32,12 +32,14 @@ android {
 }
 
 dependencies {
+    implementation(libs.appcompat) // Biblioteca de compatibilidad
+    implementation(libs.material) // Material Design
+    implementation(libs.activity) // Actividad
+    implementation(libs.constraintlayout) // Layouts de Constraint
+    implementation("androidx.core:core-splashscreen:1.0.1")// Splash Screen
+    implementation(libs.play.services.maps) // Google Maps
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    testImplementation(libs.junit) // Pruebas unitarias
+    androidTestImplementation(libs.ext.junit) // Pruebas de instrumentación
+    androidTestImplementation(libs.espresso.core) // Pruebas de UI
 }
--- app/src/main/java/cl/virginiogomez/trabajo022024/MainActivitySplash.java
+++ app/src/main/java/cl/virginiogomez/trabajo022024/MainActivitySplash.java
@@ -1,6 +1,8 @@
 package cl.virginiogomez.trabajo022024;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,19 @@ public class MainActivitySplash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_splash);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Usamos un Handler para esperar 5 segundos
+        new Handler().postDelayed(() -> {
+            // Iniciar MainActivity después de 5 segundos
+            Intent intent = new Intent(MainActivitySplash.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Llamar a finish() para que no se pueda volver a MainActivitySplash
+        }, 5000); // 5000 milisegundos = 5 segundos
     }
 }
